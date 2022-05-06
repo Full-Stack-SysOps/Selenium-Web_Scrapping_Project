fe439975192ba251b0823fd74f4b89b314475bfe
Aveek Mukherjee <Aveek.Mukherjee@myrealdata.in>
2022-05-06 10:02:07 +0530

    Final commit

--- main.py
+++ main.py
@@ -21,16 +21,12 @@ for page in range(1,3):
     for item in items:
         ratings = item.find_element_by_css_selector("div div.a-row.a-size-small").find_element_by_tag_name("span").get_attribute("aria-label")
         products_rating.append(ratings)
-        
 
         price = item.find_element_by_class_name("a-price-whole")
         products_price.append(price.text)
-      
-        
 
         price_in_inr = requests.get("https://api.frankfurter.app/latest?amount={}&from=EUR&to=INR".format(price.text))
         products_price_inr.append(price_in_inr.json()['rates']["INR"])
-        
 
         title = item.find_element_by_css_selector(".s-title-instructions-style a").text
         products_title.append(title)
@@ -43,9 +39,7 @@ for page in range(1,3):
 
         timestamp.append(datetime.now())
 
-
 df = pandas.DataFrame({"Title":products_title,"Rating":products_rating,"Price_in_EUR":products_price,"Price_in_INR":products_price_inr,"Image":products_img_link,"Product_link":products_link,"Timestamp":timestamp})
 df.to_csv("amazon_data.csv", index=False, date_format='%Y-%m-%d %H:%M:%S')
-
 
 driver.close()
