f7339e9d81a6aaba6391519a627b13f9be320395
Aveek Mukherjee <Aveek.Mukherjee@myrealdata.in>
2022-05-07 10:33:11 +0530

    Refactor code

--- main.py
+++ main.py
@@ -1,45 +1,66 @@
 from datetime import datetime
+from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+service = ChromeService(executable_path="T:\chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
 import requests
 import pandas
 
 # variables
-products_price = []
-products_title = []
-products_link = []
-products_img_link = []
-products_rating = []
-products_b_point = []
-products_price_inr = []
+product_prices = []
+product_titles = []
+product_links = []
+product_img_links = []
+product_ratings = []
+product_b_points = []
+product_price_inrs = []
 timestamp = []
-driver = webdriver.Chrome()
 
+driver.get("https://www.amazon.de/s?me=A1J99ZSJJL0INS")
 
-for page in range(1,3):
-    driver.get(f"https://www.amazon.de/s?me=A1J99ZSJJL0INS&page={page}")
-    items = driver.find_elements_by_class_name("s-asin")
-    for item in items:
-        ratings = item.find_element_by_css_selector("div div.a-row.a-size-small").find_element_by_tag_name("span").get_attribute("aria-label")
-        products_rating.append(ratings)
+def scrap_data():
+    while True:
+        items = driver.find_elements(By.CLASS_NAME, "s-asin")
+        for item in items:
+            title_class = "a-size-medium.a-color-base.a-text-normal"
+            title = item.find_element(By.CLASS_NAME, title_class).text
+            product_titles.append(title)
 
-        price = item.find_element_by_class_name("a-price-whole")
-        products_price.append(price.text)
+            link_class ="a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal"
+            prod_link = item.find_element(By.CLASS_NAME, link_class).get_attribute("href")
+            product_links.append(prod_link)
 
-        price_in_inr = requests.get(f"https://api.frankfurter.app/latest?amount={price.text}&from=EUR&to=INR")
-        products_price_inr.append(price_in_inr.json()['rates']["INR"])
+            prices = item.find_element(By.CLASS_NAME, "a-price-whole").text
+            product_prices.append(prices)
+            
+            # For Extra Credit: 1
+            price_in_inr = requests.get(f"https://api.frankfurter.app/latest?amount={prices}&from=EUR&to=INR")
+            product_price_inrs.append(price_in_inr.json()['rates']["INR"])
 
-        title = item.find_element_by_css_selector(".s-title-instructions-style a").text
-        products_title.append(title)
+            image_link = item.find_element(By.CLASS_NAME, "s-image").get_attribute("src")
+            product_img_links.append(image_link)
 
-        link = item.find_element_by_css_selector(".s-title-instructions-style a").get_attribute("href")
-        products_link.append(link)
+            ratings = item.find_element(By.CLASS_NAME, "a-icon-alt").get_attribute("innerHTML")
+            product_ratings.append(ratings)
 
-        image_link = item.find_element_by_class_name("s-image").get_attribute("src")
-        products_img_link.append(image_link)
+            timestamp.append(datetime.now())
 
-        timestamp.append(datetime.now())
+        next_btn = driver.find_element(By.CLASS_NAME, "s-pagination-next").get_attribute("href")
+        try:
+            driver.get(next_btn)
+        except Exception as e:
+            break
 
-driver.close()
+def main():
+    scrap_data()
+    df = pandas.DataFrame({"Title":product_titles,"Rating":product_ratings,"Price_in_EUR":product_prices,"Price_in_INR":product_price_inrs,"Image":product_img_links,"Product_link":product_links,"Timestamp":timestamp})
+    df.to_csv("amazon_data.csv", index=False, date_format='%Y-%m-%d %H:%M:%S')
 
-df = pandas.DataFrame({"Title":products_title,"Rating":products_rating,"Price_in_EUR":products_price,"Price_in_INR":products_price_inr,"Image":products_img_link,"Product_link":products_link,"Timestamp":timestamp})
-df.to_csv("amazon_data.csv", index=False, date_format='%Y-%m-%d %H:%M:%S')
+    driver.close()
+
+if __name__== "__main__":
+    main()
