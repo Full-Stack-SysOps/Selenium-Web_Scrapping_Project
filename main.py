601f155ccf0c162c359620a92dd3361e697f7931
Aveek Mukherjee <aveekmukherjee91@gmail.com>
2022-06-04 16:49:23 +0530

    Stop usb driver logging in console

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_experimental_option("useAutomationExtension", False)
 service = ChromeService(executable_path="T:\chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=options)
